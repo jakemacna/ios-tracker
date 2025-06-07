efc14f5e7f2d32bc8037d954d0eb28f594b93926
jakemacna <pharapoh1741@gmail.com>
2025-06-08 01:06:04 +0300

    Add files via upload

---
+++ Sources/TrackerApp/TrackerListView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct TrackerListView: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, Tracker!")
+                .navigationTitle("Tracker App")
+        }
+    }
+}
