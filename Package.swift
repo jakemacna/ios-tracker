660013a56f9852f18c75fd55523c0d44610e757e
jakemacna <pharapoh1741@gmail.com>
2025-06-08 01:05:20 +0300

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "TrackerApp",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .executable(
+            name: "TrackerApp",
+            targets: ["TrackerApp"]
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "TrackerApp",
+            path: "Sources/TrackerApp"
+        )
+    ]
+)
